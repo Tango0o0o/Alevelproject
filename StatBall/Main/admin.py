667898c4826371c6cd6d25ce06380475c407a3d7
Tango0o0o <dacostawalanyo49@gmail.com>
2025-07-28 20:12:04 +0100

    Created user class/model + migrations Allowed SignUpForm data to be passed into model Written function to save user into database Written validation functions for email and password Added model to admin panel Updated gitignore to include Venv

--- StatBall/Main/admin.py
+++ StatBall/Main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+
+# This adds the model to the admin panel so I can see the objects created from them
+admin.site.register(User)
